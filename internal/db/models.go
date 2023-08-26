1414e2d949aa61f996bc425fee48da3335a95567
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2023-08-26 19:48:03 +0300

    now exporting db models to transport dto

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,9 +1,17 @@
 package repository
 
+import "time"
+
 type Segment struct {
-	Slug string `json:"slug" db:"slug"`
+	Slug string `json:"slug" db:"slug" validate:"required"`
 }
 type User struct {
 	Id       int    `json:"id" db:"id"`
 	Username string `json:"username" db:"username"`
+}
+type HistoryRow struct {
+	UserId int `json:"user_id" db:"user_id" `
+	Segment
+	Operation  string    `json:"operation" id:"operation"`
+	UpdateTime time.Time `json:"update_time" db:"update_time"`
 }
